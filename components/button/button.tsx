bde1b9fdf4513f0ce9396787f12a9ce5b612f4f7
Albin Hoti <albin.hoti@mgb.ch>
2023-09-12 20:48:35 +0200

    chore: init repo & install storybook

---
+++ components/button/button.tsx
@@ -0,0 +1,42 @@
+import clsx from 'clsx';
+
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({ primary = false, size = 'medium', backgroundColor, label, ...props }: ButtonProps) => {
+  const mode = primary ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-800';
+  const sizeClasses = {
+    small: 'px-2 py-1 text-sm',
+    medium: 'px-3 py-2 text-base',
+    large: 'px-4 py-3 text-lg',
+  }[size];
+
+  return (
+    <button type="button" className={clsx(sizeClasses, mode)} {...props}>
+      {label}
+    </button>
+  );
+};
