bfbd66524f31cad2ad878341ce5e4315c9623b6c
Albin Hoti <albin.hoti@mgb.ch>
2023-09-18 09:59:21 +0200

    chore: eslint import/resolver fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,12 @@ const config = {
   rules: {
     'prettier/prettier': 0,
   },
-  extends: ['plugin:storybook/recommended', '@smartive/eslint-config'],
+  settings: {
+    'import/resolver': {
+      typescript: true,
+    },
+  },
+  extends: ['plugin:storybook/recommended', '@smartive/eslint-config', 'plugin:import/typescript'],
 };
 
 module.exports = config;
