541eb40a4ec29172269364f8acbb9e0aa10aa60f
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2023-10-05 14:59:51 +0200

    feat: button

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -13,16 +13,28 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {},
+  argTypes: {
+    onClick: { action: 'clicked' }
+  },
 } satisfies Meta<typeof Button>;
-
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
+export const ButtonPrimary: Story = {
   args: {
-    primary: true,
     label: 'Button',
+  },
+};
+export const ButtonSecondary: Story = {
+  args: {
+    label: 'Button',
+    color: 'secondary',
+  },
+};
+export const ButtonGradient: Story = {
+  args: {
+    label: 'Button',
+    color: 'gradient',
   },
 };
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,9 +2,9 @@ import clsx from 'clsx';
 
 interface ButtonProps {
   /**
-   * Is this the principal call to action on the page?
+   * Button colors
    */
-  primary?: boolean;
+  color?: 'primary' | 'secondary' | 'gradient';
   /**
    * How large should the button be?
    */
@@ -17,22 +17,56 @@ interface ButtonProps {
    * Optional click handler
    */
   onClick?: () => void;
+  /**
+   * Optional disable button
+   */
+  disabled?: boolean,
 }
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary = false, size = 'medium', label, ...props }: ButtonProps) => {
-  const mode = primary ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-800';
-  const sizeClasses = {
-    small: 'px-2 py-1 text-sm',
-    medium: 'px-3 py-2 text-base',
-    large: 'px-4 py-3 text-lg',
+const buttonClasses = 'w-full border border-solid border-slate-200 rounded-md text-white text-base outline-none active:border-4 leading-none disabled:pointer-events-none disabled:border-1 disabled:opacity-75'
+
+export const Button = ({ 
+  color = 'primary', 
+  size = 'medium', 
+  label,
+  disabled = false,
+  onClick,
+ }: ButtonProps) => {
+
+    const sizeClasses = {
+    small: {
+      text: 'text-sm',
+      container: 'px-2 py-1',
+    },
+    medium: {
+      text: 'text-base',
+      container: 'px-4 py-2',
+    },
+    large: {
+      text: 'text-lg',
+      container: 'px-8 py-4',
+    },
   }[size];
 
+  const colorClasses = {
+    primary: 'bg-violet-600 focus:border-violet-200 hover:bg-violet-700',
+    secondary: 'bg-slate-600 focus:border-slate-200 hover:bg-slate-700',
+    gradient: 'bg-gradient-to-r from-pink-500 to-violet-600 focus:border-violet-200 hover:bg-gradient-to-r hover:from-pink-600 hover:to-violet-700',
+  }[color]
+
   return (
-    <button type="button" className={clsx(sizeClasses, mode)} {...props}>
-      {label}
-    </button>
+      <button
+        type="button" 
+        aria-label={label}
+        onClick={onClick}
+        disabled={disabled}
+        className={clsx(buttonClasses, colorClasses)}>
+          <div className={`flex place-content-center items-center ${sizeClasses.container}`}>
+            <div className={`fwhitespace-nowrap ${sizeClasses.text}`}>{label}</div>
+          </div>
+      </button>
   );
 };
