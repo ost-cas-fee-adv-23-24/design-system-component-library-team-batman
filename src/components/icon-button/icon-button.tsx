6c5aa621afa54cc2818d23f60f473c5dc06374d4
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2023-10-10 17:09:49 +0200

    fix: disabled added - cursor pointer removed not needed for button

--- src/components/icon-button/icon-button.tsx
+++ src/components/icon-button/icon-button.tsx
@@ -21,12 +21,13 @@ import { Icon, IIconProps } from '../icon';
   onClick?: () => React.ComponentProps<'button'>['onClick'];
 }
 
-export const IconButton = ({ variant, className, onClick, }: IIconButtonProps) => {
+export const IconButton = ({ variant, className, disabled = false, onClick, }: IIconButtonProps) => {
   return (
     <button 
-      className={cn("cursor-pointer rounded-full p-s bg-base-600 transition-color duration-300 hover:bg-base-700 focus:outline-4 focus:bg-base-700 hover:outline-[3px] outline-offset-0", className)} 
+      className={cn("rounded-full p-s bg-base-600 transition-color duration-300 hover:bg-base-700 focus:outline-4 focus:bg-base-700 hover:outline-[3px] outline-offset-0", className)} 
       aria-label={variant} 
       onClick={onClick}
+      disabled={disabled}
     >
       <Icon variant={variant} size='s' className='fill-white'/>
     </button>
