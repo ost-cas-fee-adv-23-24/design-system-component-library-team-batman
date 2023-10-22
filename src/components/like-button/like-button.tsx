ec7d2d6394d9b82b26b89e6f0e3c58bc3a630253
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2023-10-22 09:23:36 +0200

    refactor: code review last fixes

--- src/components/like-button/like-button.tsx
+++ src/components/like-button/like-button.tsx
@@ -58,16 +58,13 @@ export const LikeButton = ({
     }
   };
 
-  // const [isActive, setIsActive] = useState(false)
   const style = {
     unliked: {
       icon: {
         color: 'fill-base-600',
-        active: 'fill-accent-600',
       },
       text: {
         color: 'text-base-600',
-        active: 'text-accent-600',
       },
     },
     liked: {
@@ -81,43 +78,29 @@ export const LikeButton = ({
     likes: {
       icon: {
         color: 'fill-accent-600',
-        active: 'fill-accent-600',
       },
       text: {
         color: 'text-accent-600',
-        active: 'text-accent-900',
       },
     },
   }[(isLiked && 'liked') || variant];
 
   const iconLabel = isLiked ? 'Liked' : likes > 1 ? 'Likes' : 'Like';
-  // const iconHover = style.icon?.active as string
-  // const textover = style.text?.active as string
 
-  // const handleSetActive = (state: boolean) => {
-  //   if (variant === 'liked') {
-  //     return
-  //   }
-  //   setIsActive(state)
-  // }
   return (
     <button
       type="button"
       aria-label={iconLabel}
       onClick={handleLike}
       disabled={disabled}
-      className={cn('flex rounded-m p-xs transition-colors duration-300 ease-in-out', 'group hover:bg-accent-50')}
-      // onMouseOver={() => handleSetActive(true)}
-      // onMouseLeave={() => handleSetActive(false)}
-      // onFocus={() => handleSetActive(true)}
-      // onBlur={() => handleSetActive(false)}
+      className="group flex rounded-m pb-xs pl-s pr-s pt-xs transition-colors duration-300 ease-in-out hover:bg-accent-50"
     >
       <Icon
         size="s"
-        className={cn(style.icon.color, 'group hover:fill-accent-600')}
+        className={cn(style.icon.color, 'group-hover:fill-accent-600')}
         variant={variant === 'unliked' && !isLiked ? 'heart' : 'heart-filled'}
       />
-      <Label size="m" className={cn('group ml-xs cursor-pointer hover:text-accent-600', style.text?.color)}>
+      <Label size="m" className={cn('ml-xs cursor-pointer group-hover:text-accent-600', style.text?.color)}>
         {likes > 0 ? `${likes.toString()} ${iconLabel}` : iconLabel}
       </Label>
     </button>
