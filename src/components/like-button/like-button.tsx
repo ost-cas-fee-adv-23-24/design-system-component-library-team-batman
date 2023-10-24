2b3545225af747ce13fcbd1ea8a57b6c43e728a8
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2023-10-24 19:14:13 +0200

    fix: reviews fixes

--- src/components/like-button/like-button.tsx
+++ src/components/like-button/like-button.tsx
@@ -10,10 +10,12 @@ export interface LikeButtonProps {
    */
   isLikedByUser?: boolean;
   /**
-   *
-   * actions
+   * increment Likes
    */
   onLikeAdd?: () => Promise<void>;
+  /**
+   * decrement Likes
+   */
   onLikeRemove?: () => Promise<void>;
   /**
    * likes counter
@@ -38,23 +40,28 @@ export const LikeButton = ({
 }: LikeButtonProps) => {
   const [variant, setVariant] = useState<'unliked' | 'liked' | 'likes'>(likes > 0 ? 'likes' : 'unliked');
   const [isLiked, setIsLiked] = useState(isLikedByUser);
+  const [likesCount, setLikesCount] = useState(likes);
   const [isDisabled, setIsDisabled] = useState(disabled);
 
   const handleLike = async () => {
     if (isLiked) {
       setIsLiked(false);
-      setIsDisabled(false);
+      setIsDisabled(true);
+      setLikesCount(likesCount - 1);
       await onLikeRemove?.();
+      setIsDisabled(false);
     } else {
       setIsLiked(true);
       setIsDisabled(true);
       setVariant('liked');
+      setLikesCount(likesCount + 1);
       await onLikeAdd?.();
       new Promise((resolve) =>
         setTimeout(() => {
           setVariant('likes');
+          setIsDisabled(false);
           resolve(true);
-        }, 600),
+        }, 1000),
       );
     }
   };
@@ -62,7 +69,7 @@ export const LikeButton = ({
   const style = {
     unliked: {
       icon: {
-        color: 'fill-base-600',
+        color: 'fill-base-500',
       },
       text: {
         color: 'text-base-600',
@@ -70,7 +77,7 @@ export const LikeButton = ({
     },
     liked: {
       icon: {
-        color: 'fill-accent-600',
+        color: 'fill-accent-500',
       },
       text: {
         color: 'text-accent-900',
@@ -78,15 +85,15 @@ export const LikeButton = ({
     },
     likes: {
       icon: {
-        color: 'fill-accent-600',
+        color: 'fill-accent-500',
       },
       text: {
-        color: 'text-accent-600',
+        color: 'text-accent-900',
       },
     },
-  }[(isLiked && 'liked') || variant];
+  }[variant];
 
-  const iconLabel = isLiked ? 'Liked' : likes > 1 ? 'Likes' : 'Like';
+  const iconLabel = variant === 'liked' ? 'Liked' : likesCount > 0 ? `${likesCount.toString()} Likes` : 'Like';
 
   return (
     <button
@@ -102,19 +109,20 @@ export const LikeButton = ({
     >
       <Icon
         size="s"
-        className={cn(style.icon.color, !isDisabled && 'group-hover:fill-accent-600')}
-        variant={variant === 'unliked' && !isLiked ? 'heart' : 'heart-filled'}
+        className={cn(style.icon.color, 'group-hover:fill-accent-500', 'transition-colors duration-300 ease-in-out')}
+        variant={variant === 'unliked' || !isLiked ? 'heart' : 'heart-filled'}
       />
       <Label
         size="m"
         className={cn(
           'ml-xs',
+          'transition-colors duration-300 ease-in-out',
           !isDisabled && 'group-hover:text-accent-600',
           style.text?.color,
           !isDisabled && 'cursor-pointer',
         )}
       >
-        {likes > 0 ? `${likes.toString()} ${iconLabel}` : iconLabel}
+        {iconLabel}
       </Label>
     </button>
   );
