a3169af2172241de9f99b9a899155db384186ae6
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2023-10-26 15:06:46 +0200

    feat: link first commit

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export * from './components/form/textarea';
 export * from './components/icon';
 export * from './components/icon-button';
 export * from './components/like-button';
+export * from './components/link';
 export * from './components/typography/heading';
 export * from './components/typography/label';
 export * from './components/typography/paragraph';
