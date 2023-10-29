a77ddbdd84e4b76227acc505f5f23b3ac55029ad
Ricardo Vögeli <ricardo.voegeli@ost.ch>
2023-10-29 10:31:44 +0100

    fix: link undelrine added and space fixed

--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -25,27 +25,31 @@ export interface LinkProps {
 
 export const Link = ({ variant = 'link', disabled = false, text, onClick }: LinkProps) => {
   return (
-    <button type="button" aria-label={text} className="group flex" onClick={onClick}>
-      {variant !== 'link' && (
-        <Icon
-          size="xs2"
-          variant={variant}
-          className={cn(
-            'mr-xs fill-primary-600 group-hover:fill-primary-900',
-            disabled && 'fill-base-600 group-hover:fill-base-400',
-          )}
-        />
-      )}
-      <Label
-        size="s"
-        className={cn(
-          'text-primary-600 group-hover:text-primary-900',
-          disabled && 'text-base-600 group-hover:text-base-400',
-          !disabled && 'cursor-pointer',
+    <button type="button" aria-label={text} className="group" onClick={onClick}>
+      <div className="flex">
+        {variant !== 'link' && (
+          <Icon
+            size="xs2"
+            variant={variant}
+            className={cn(
+              'mr-xxs fill-primary-600 group-hover:fill-primary-900',
+              disabled && 'fill-base-600 group-hover:fill-base-400',
+            )}
+          />
         )}
-      >
-        {text}
-      </Label>
+        <Label
+          size="s"
+          className={cn(
+            'text-primary-600',
+            variant !== 'link' && 'group-hover:text-primary-900',
+            disabled && 'text-base-600 group-hover:text-base-400',
+            !disabled && 'cursor-pointer',
+          )}
+        >
+          {text}
+        </Label>
+      </div>
+      {variant === 'link' && <span className="block border border-primary-600 group-hover:border-primary-200" />}
     </button>
   );
 };
