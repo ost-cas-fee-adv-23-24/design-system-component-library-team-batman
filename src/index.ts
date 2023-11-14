d7026ee962929ba488a7e05bd33e5454bdf905d3
risuiar2023 <145476582+risuiar2023@users.noreply.github.com>
2023-11-14 18:10:34 +0100

    feat: copy button (#20)
    Copy button added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './components/button';
 export * from './components/comment-button';
+export * from './components/copy-button';
 export * from './components/form/file-upload';
 export * from './components/form/hint';
 export * from './components/form/input';
